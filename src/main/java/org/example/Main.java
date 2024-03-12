291ebb10b358f02c02819d7ee4a3ad1dc12ac7cf
BaselQq <73541125+BaselQq@users.noreply.github.com>
2024-03-12 11:13:57 +0100

    add figletfont and print hello world in ascii

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
+
+
     public static void main(String[] args) {
-	    System.out.println("Hello World");
+
+        figlet("Hello World");
+    }
+
+
+    public static void figlet(String text) {
+        String asciiArt2 = null;
+
+        try {
+            asciiArt2 = FigletFont.convertOneLine(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(asciiArt2);
     }
 }
